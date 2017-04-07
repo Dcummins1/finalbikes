a89861031f22fdf305d2bd9b314119d260b820c4
Dcummins1 <daniel.cummins00@gmail.com>
2017-04-07 17:01:46 +0100

    New file structure

--- flaskproject/app.py
+++ flaskproject/app.py
@@ -1,49 +1,65 @@
 import os.path
-
 from flask import Flask, Response
+import sys
+import logging
+import pymysql
+import os
+import requests
+import json
+import time
+import plotly.plotly as py
+import plotly.graph_objs as go
+import matplotlib.pyplot as plt
+from datetime import datetime
 
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
 
-def root_dir():  # pragma: no cover
+def root_dir():  
     return os.path.abspath(os.path.dirname(__file__))
 
 
-def get_file(filename):  # pragma: no cover
+def get_file(filename):
     try:
         src = os.path.join(root_dir(), filename)
-        # Figure out how flask returns static files
-        # Tried:
-        # - render_template
-        # - send_file
-        # This should not be so non-obvious
+        
         return open(src).read()
     except IOError as exc:
         return str(exc)
 
 
-@app.route('/', methods=['GET'])
-def metrics():  # pragma: no cover
-    content = get_file('dublinbikes.html')
+@app.route('/', methods=['GET', 'POST'])
+def metrics():  
+    content = get_file('templates/DublinBikes.html')
     return Response(content, mimetype="text/html")
 
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def get_resource(path):  # pragma: no cover
-    mimetypes = {
-        ".css": "text/css",
-        ".html": "text/html",
-        ".js": "application/javascript",
-    }
-    complete_path = os.path.join(root_dir(), path)
-    ext = os.path.splitext(path)[1]
-    mimetype = mimetypes.get(ext, "text/html")
-    content = get_file(complete_path)
-    return Response(content, mimetype=mimetype)
+# @app.route('/', defaults={'path': ''})
+# @app.route('/<path:path>')
+# def get_resource(path):  
+#     mimetypes = {
+#         ".css": "text/css",
+#         ".html": "text/html",
+#         ".js": "application/javascript",
+#     }
+#     complete_path = os.path.join(root_dir(), path)
+#     ext = os.path.splitext(path)[1]
+#     mimetype = mimetypes.get(ext, "text/html")
+#     content = get_file(complete_path)
+#     return Response(content, mimetype=mimetype)
 
-
-if __name__ == '__main__':  # pragma: no cover
-    app.run(port=80)
+@app.route("/availablebikes/<int:stand_no>")
+def avail_bikes(stand_no):        
+    #if test == True:
+        #x = int(input('enter stop number'))
+        #sql = "SELECT * From dublin_bikes_dynamic WHERE number = {0} ORDER BY last_update DESC Limit 1;".format(stand_no)
+        #curs.execute(sql)
+        #for row in curs.fetchall():
+            #return 'Stand num: %d' % 4
+    return 'hello &d' % stand_no
+        
+        
+if __name__ == '__main__':  
+    app.run(port=8080)
