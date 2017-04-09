3bd63a0fca15348b33ecc99a89704376e3288540
Eoghan O'Neill <eoghan.o-neill.2@ucdconnect.ie>
2017-04-09 20:36:09 +0100

    "Full commit
     create dataTable with json sample
    "

---
+++ flaskproject/queries2.py
@@ -0,0 +1,56 @@
+import sys
+import logging
+import src.rds_config
+import pymysql
+#import os
+import requests
+import json
+import time
+import plotly.plotly as py
+import plotly.graph_objs as go
+import matplotlib.pyplot as plt
+import datetime
+ 
+
+rds_host = src.rds_config.db_endpoint
+name = src.rds_config.db_username
+password = src.rds_config.db_password
+db_name = src.rds_config.db_name
+port = 3306
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+APIKEY = "58c66e96f312aedb78f7f726e5da74ec7ade7e33"
+NAME = "Dublin"
+STATIONS_URL = "https://api.jcdecaux.com/vls/v1/stations"
+try:
+    conn = pymysql.connect(rds_host, user=name,
+    passwd=password, db=db_name, connect_timeout=10000)
+    test = True
+    curs = conn.cursor()
+except:
+    logger.error("ERROR: Unexpected error: Could not connect to MySql instance.")
+    test = False
+    sys.exit()
+    
+while test == True:
+    x = int(input('enter stop number'))
+#     sql = "SELECT TOP 3 last_updated FROM dublin_bikes_dynamic;"
+    sql = "SELECT * From dublin_bikes_dynamic WHERE number = {0} ORDER BY last_update DESC Limit 1;".format(x)
+#     sql = "SELECT top 288 available_bikes, available_bike_stands From dublin_bikes_dynamic WHERE number = {0} ORDER BY last_update DESC Limit 1;".format(x)
+    curs.execute(sql)
+    for row in curs.fetchall():
+#         print (datetime.datetime.fromtimestamp(row[6]/1000))
+        print(row)
+    
+    
+    
+    labels = 'Available Bikes {0}'.format(row[4]), 'Unavailable Bikes {0}'.format(row[5])
+    sizes = [row[4], row[5]]
+    colors = ['yellowgreen', 'teal']
+    plt.title('{0}, ({1}) \n Status:{2} \n {3}'.format(row[1], row[0], row[2], 3)) 
+    
+    plt.pie(sizes, labels=labels, colors=colors, autopct=None ,startangle=140)
+    patches, texts = plt.pie(sizes, colors=colors)
+    plt.legend(patches, labels, loc="best") 
+    plt.axis('equal')
+    plt.show()
--- src/livescrape.py
+++ src/livescrape.py
@@ -1,6 +1,5 @@
 import requests
 import json
-from pprint import pprint
 
 APIKEY = "58c66e96f312aedb78f7f726e5da74ec7ade7e33"
 NAME = "Dublin"
@@ -8,5 +7,5 @@ STATIONS_URI = "https://api.jcdecaux.com/vls/v1/stations"
 r = requests.get(STATIONS_URI, params={"apiKey": APIKEY,
  "contract": NAME})
 
-a = (json.loads(r.text)[0])
+a = json.dumps(json.JSONDecoder().decode(r.text))
 print(a)
