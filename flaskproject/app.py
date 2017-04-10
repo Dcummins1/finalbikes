ecf807bda3609178d6be37bb40bb1bfb0e04f944
Eoghan O'Neill <eoghan.o-neill.2@ucdconnect.ie>
2017-04-10 08:53:01 +0100

    "Further Google Charts Investigation
    Area, Bar, show Table"

--- flaskproject/app.py
+++ flaskproject/app.py
@@ -1,64 +1,25 @@
 import os.path
-from flask import Flask, Response
-import sys
-import logging
-import pymysql
-import os
-import requests
-import json
-import time
-import plotly.plotly as py
-import plotly.graph_objs as go
-import matplotlib.pyplot as plt
-from datetime import datetime
-
+from flask import Flask, Response, g, render_template, jsonify
+from flaskproject.queries import availability
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+app.debug=True
+ 
 
 
-def root_dir():  
-    return os.path.abspath(os.path.dirname(__file__))
+@app.route('/')
+def root():  
+    return render_template('DublinBikes.html')
+    
 
 
-def get_file(filename):
-    try:
-        src = os.path.join(root_dir(), filename)
-        
-        return open(src).read()
-    except IOError as exc:
-        return str(exc)
+@app.route("/test/<int:stand_no>", methods=['GET'])
+def avail(stand_no):      
+    #print("stand no is: %d" % stand_no)
+    return availability(stand_no)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def metrics():  
-    content = get_file('templates/DublinBikes.html')
-    return Response(content, mimetype="text/html")
-
-
-# @app.route('/', defaults={'path': ''})
-# @app.route('/<path:path>')
-# def get_resource(path):  
-#     mimetypes = {
-#         ".css": "text/css",
-#         ".html": "text/html",
-#         ".js": "application/javascript",
-#     }
-#     complete_path = os.path.join(root_dir(), path)
-#     ext = os.path.splitext(path)[1]
-#     mimetype = mimetypes.get(ext, "text/html")
-#     content = get_file(complete_path)
-#     return Response(content, mimetype=mimetype)
-
-@app.route("/availablebikes/<int:stand_no>")
-def avail_bikes(stand_no):        
-    #if test == True:
-        #x = int(input('enter stop number'))
-        #sql = "SELECT * From dublin_bikes_dynamic WHERE number = {0} ORDER BY last_update DESC Limit 1;".format(stand_no)
-        #curs.execute(sql)
-        #for row in curs.fetchall():
-            #return 'Stand num: %d' % 4
-    return 'hello &d' % stand_no
         
         
 if __name__ == '__main__':  
